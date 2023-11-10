277edd48dc4dee0d9b469e12a1bdfb95aecf8cce
Tima test <timmy12122022@gmail.com>
2023-11-10 19:25:44 +0700

    Initial commit

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -1,9 +1,5 @@
 package ru.netology.page;
 
-import ru.netology.data.DataHelper;
-
-import static com.codeborne.selenide.Condition.visible;
-
 public class DashboardPage {
     public DashboardPage() {
     }
